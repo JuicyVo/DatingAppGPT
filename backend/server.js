043e1748b9365f462760a233f1be5f61f9275de9
William <Williamtviet@hotmail.com>
2023-09-05 02:45:11 -0700

    added functionality to allow to see if user for like is a bot, making botsa instant match

--- backend/server.js
+++ backend/server.js
@@ -109,27 +109,110 @@ app.post('/get-user-id', (req, res) => { //god damn this was a pain
   }
 });
 
+app.post('/create-like', (req, res) => {
+  try {
+    const { likerId, likedUserId } = req.body;
+
+    // Check if the target user is a bot
+    const checkBotQuery = 'SELECT isBot FROM UserProfile WHERE userId = $1;';
+    const values = [likedUserId];
+
+    client.query(checkBotQuery, values, (error, result) => {
+      if (error) {
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while checking if the user is a bot.' });
+      } else {
+        const isBot = result.rows[0].isbot;
+        console.log (result.rows[0].isbot)
+        console.log(`User ${likerId} is liking User ${likedUserId}. Is target user a bot? ${isBot ? 'Yes' : 'No'}`);
+
+        if (isBot) {
+          // Create a mutual like if the target user is a bot
+          const createLikeQuery = 'INSERT INTO Likes (likerId, likedUserId) VALUES ($1, $2), ($2, $1);';
+          const likeValues = [likerId, likedUserId];
+          console.log ("IS A BOT")
+          client.query(createLikeQuery, likeValues, (likeError) => {
+            if (likeError) {
+              console.error(likeError);
+              res.status(500).json({ error: 'An error occurred while creating a mutual like.' });
+            } else {
+              res.json({ message: 'Like created successfully.' });
+              console.log(`User ${likerId} is liking User ${likedUserId}. Is target user a bot? ${isBot ? 'Yes' : 'No'}`);
+            }
+          });
+        } else {
+          // If the target user is not a bot, create a one-way like
+          const createLikeQuery = 'INSERT INTO Likes (likerId, likedUserId) VALUES ($1, $2);';
+          const likeValues = [likerId, likedUserId];
+          console.log ("ISNT A BOT")
+
+          client.query(createLikeQuery, likeValues, (likeError) => {
+            if (likeError) {
+              console.error(likeError);
+              res.status(500).json({ error: 'An error occurred while creating a like.' });
+            } else {
+              res.json({ message: 'Like created successfully.' });
+            }
+          });
+        }
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while creating a like.' });
+  }
+});
+
+
 
 app.post('/create-match', (req, res) => {
   try {
     const { user1Id, user2Id } = req.body;
 
-    const query = 'INSERT INTO Matches (user1Id, user2Id) VALUES ($1, $2) RETURNING *;';
+    console.log(`Received request to create match between User ${user1Id} and User ${user2Id}`);
+
+    // Check if both users have liked each other
+    const checkLikeQuery = 'SELECT EXISTS (SELECT 1 FROM Likes WHERE likerId = $1 AND likedUserId = $2) AS user1LikesUser2, ' +
+      'EXISTS (SELECT 1 FROM Likes WHERE likerId = $2 AND likedUserId = $1) AS user2LikesUser1;';
     const values = [user1Id, user2Id];
-    
-    client.query(query, values, (error, result) => {
+
+    client.query(checkLikeQuery, values, (error, result) => {
       if (error) {
-        console.error('Error creating a match:', error);
-        res.status(500).json({ error: 'An error occurred while creating a match' });
+        console.error('Error while checking likes:', error);
+        res.status(500).json({ error: 'An error occurred while checking likes between users.' });
       } else {
-        const match = result.rows[0];
-        console.log('Match created:', match);
-        res.json({ match });
+        const user1LikesUser2 = result.rows[0].user1likesuser2;
+        const user2LikesUser1 = result.rows[0].user2likesuser1;
+
+        console.log(`Received request to create match between User ${user1Id} and User ${user2Id}`);
+        console.log(`User ${user1Id} likes User ${user2Id}: ${user1LikesUser2}`);
+        console.log(`User ${user2Id} likes User ${user1Id}: ${user2LikesUser1}`);
+        
+
+        if (user1LikesUser2 && user2LikesUser1) {
+          // Both users have liked each other, create a match
+          const createMatchQuery = 'INSERT INTO Matches (user1Id, user2Id) VALUES ($1, $2);';
+          const matchValues = [user1Id, user2Id];
+
+          client.query(createMatchQuery, matchValues, (matchError) => {
+            if (matchError) {
+              console.error('Error creating match:', matchError);
+              res.status(500).json({ error: 'An error occurred while creating a match.' });
+            } else {
+              console.log(`Match created between User ${user1Id} and User ${user2Id}`);
+              res.json({ message: 'Match created successfully.' });
+            }
+          });
+        } else {
+          // Users have not liked each other, cannot create a match
+          console.log(`Users ${user1Id} and ${user2Id} did not like each other.`);
+          res.status(400).json({ error: 'Both users must like each other to create a match.' });
+        }
       }
     });
   } catch (error) {
-    console.error('Error creating a match:', error);
-    res.status(500).json({ error: 'An error occurred while creating a match' });
+    console.error('Exception caught:', error);
+    res.status(500).json({ error: 'An error occurred while creating a match.' });
   }
 });
 
