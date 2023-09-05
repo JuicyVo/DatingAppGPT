f486eb4a5911b766c0dc319c731df2fd4e5079ea
William <Williamtviet@hotmail.com>
2023-09-04 20:00:37 -0700

    now has handleLike functionality and makes matches using both userIds

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -19,6 +19,9 @@ export default function ProfileCard() {
       });
   }, [])
 
+  // console.log (user)//this works
+  const constructedEmail = `${user.given_name}.${user.family_name}@example.com`;
+
  
 
   const handleNextProfile = () => {
@@ -32,11 +35,46 @@ export default function ProfileCard() {
     }
   };
 
+ const handleLike = (constructedEmail, likedUserId) => {
+  axios
+    .post('http://localhost:8000/get-user-id', {
+      email: constructedEmail,
+    })
+    .then(response => {
+      const user1Id = response.data.userId;
+      const user2Id= likedUserId
+      console.log(`User with ID ${user1Id} likes user with ID ${likedUserId}`);
+      
+      axios
+        .post('http://localhost:8000/create-match', {
+          user1Id,
+          user2Id,
+        })
+        .then (matchResponse => {
+          const match = matchResponse.data.match;
+          console.log ('Match created', match)
+        })
+        .catch(error =>{
+          console.log ('Error creating a match', error)
+        })
+
+
+    })
+    .catch(error => {
+      console.log('Error getting userId:', error);
+    });
+};
+
   if (users.length === 0) {
     return <div>Loading...</div>;
   }
 
   const currentProfile = users[currentIndex];
+
+// console.log(`${user.given_name}.${user.family_name}@example.com`)
+// console.log(currentProfile.userid)
+
+
 
   return (
     <div className="profile-card">
@@ -53,7 +91,9 @@ export default function ProfileCard() {
       </div>
       <div className="like-dislike">
         <button>Dislike</button>
-        {/* <button onClick={handleLike}>Like</button> */}
+        
+        <button onClick={() => handleLike(`${user.given_name}.${user.family_name}@example.com`, currentProfile.userid)}>Like</button>
+
       </div>
     </div>
   );
